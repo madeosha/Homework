7650e513222aecd8933917e79f4964fd04ae3276
madeosha <lojkina95@yandex.ru>
2023-04-17 13:52:31 +0500

    Маленькие исправления в hw-3

--- hw-3.js
+++ hw-3.js
@@ -1,11 +1,12 @@
 // задание 1
 let password = 'зевс';
 let check = prompt('Введите пароль');
-if (password = 'зевс') {
+if (password === check) {
     alert ("Пароль введен верно");
 } else {
     alert ("Пароль введен неверно");
 }
+
 
 // задание 2
 let c = 2;
@@ -34,38 +35,22 @@ alert(a + b);
 let monthNumber = Number(prompt('Введите номер месяца, чтобы узнать сезон'));
 switch (monthNumber) {
     case 12:
-        console.log('Зима');        
-        break;
     case 1:
-        console.log('Зима');        
-        break;
     case 2:
         console.log('Зима');        
         break;
     case 3:
-        console.log('Весна');        
-        break;
     case 4:
-        console.log('Весна');        
-        break;
     case 5:
         console.log('Весна');        
         break;
     case 6:
-        console.log('Лето');        
-        break;
     case 7:
-        console.log('Лето');        
-        break;
     case 8:
         console.log('Лето');        
         break;
     case 9:
-        console.log('Осень');        
-        break;
     case 10:
-        console.log('Осень');        
-        break;
     case 11:
         console.log('Осень');        
         break;
