38451a9929a45163c4cb1725e005fa40b2518178
madeosha <lojkina95@yandex.ru>
2023-04-23 00:17:07 +0500

    Сделала 8 задание

---
+++ script.js
@@ -0,0 +1,12 @@
+let monthNumber = Number(prompt('Введите номер месяца, чтобы узнать сезон'));
+if (monthNumber >=  1 && monthNumber <= 2 || monthNumber == 12) {
+    console.log("Зима")    
+} else if (monthNumber >=  3 && monthNumber <= 4 || monthNumber == 5) {
+    console.log("Весна") 
+} else if (monthNumber >=  6 && monthNumber <= 7 || monthNumber == 8) {
+    console.log("Лето") 
+} else if (monthNumber >=  9 && monthNumber <= 10 || monthNumber == 11) {
+    console.log("Осень") 
+} else {
+    console.log("Вы ввели что-то не то")
+}
